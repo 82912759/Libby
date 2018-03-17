5ef194efc1f008688a3f768879e26d1258081251
habbo <habbo@jans-mac-mini.fritz.box>
2018-03-17 23:11:25 +0100

    Validate::isOnline

--- src/Validate.php
+++ src/Validate.php
@@ -1,11 +1,39 @@
-<?php 
+<?php
 /**
- * 
+ * @author Jan Habbo Brüning <jan.habbo.bruening@gmail.com> 
+ * @date 2018-03-17
  */
 
 namespace Libby;
 
 class Validate {
 	
+	/**
+	 * Validate email
+	 */
+	public static function email ( $email, array $params = null ) {
+		
+		if (!preg_match('#^(.*?)\@(.*?)\.([a-z0-9]{2,})$#i', $email)) {
+			throw new Exception\Input('ExceptionValidateEmail', $email);
+		}
+		
+		if (empty($params['skipDns']) AND self::isOnline()) {
+		
+			list($mbox, $server)	=	explode('@', $email);
+		
+			if (!checkdnsrr($server, 'MX') AND !checkdnsrr($server, 'A')) {
+				throw new Exception\Input('ExceptionValidateEmailDns', $email);
+			}
+		}
+	}
+	
+	
+	/**
+	 * Validate if php is connected to the internet 
+	 */
+	public static function isOnline ( ) {
+		
+		return @fsockopen("www.example.com", 80);		
+	}
 }
 ?>
