0893e3c93edba33ee0f82e5ccb7f3e35076dc9dc
Jan Habbo Brüning <hbruening@itemkg.de>
2018-05-11 09:34:00 +0200

    added parser->setHtml()

--- src/Html/Parser.php
+++ src/Html/Parser.php
@@ -7,4 +7,13 @@ namespace Libby\Html;
 
 class Parser {
     
+    protected $html;
+    
+    /**
+     * Set html context
+     */    
+    public function setHtml ( $html ) {
+        
+        $this->html = $html;
+    }
 }
