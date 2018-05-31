8324d212d2a2ab67db0977e340fbdf4942747bae
Jan Habbo Brüning <hbruening@itemkg.de>
2018-05-31 16:05:16 +0200

    added Http\Request->get()

--- src/Http/Request.php
+++ src/Http/Request.php
@@ -24,6 +24,33 @@ class Request {
 	/**
 	 * 
 	 */
+    public function get ( array $params ) {
+        
+        $ch = curl_init($this->uri);
+        
+        if (!empty($params['username'])) {
+            curl_setopt($ch, CURLOPT_USERPWD, $params['username'] . ':' . $params['password']);
+            curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        }
+        
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 60);
+        curl_setopt($ch, CURLOPT_HEADER, true);
+        
+        $result = curl_exec($ch);
+
+        $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
+		$header = substr($result, 0, $header_size);
+		$body = substr($result, $header_size);
+		
+		return new Response(explode("\n\r", $header), $body);
+        
+    }
+	
+	
+	/**
+	 * 
+	 */
 	public function post ( $data = null, array $params = null ) {
 		
 		$ch = curl_init($this->uri);
@@ -33,6 +60,11 @@ class Request {
 		if (!empty($data)) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
 		}		
+		
+		if (!empty($params['username'])) {
+		    curl_setopt($ch, CURLOPT_USERPWD, $params['username'] . ':' . $params['password']);
+		    curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+		}
 		
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
